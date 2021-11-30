e44152762aa7d85f9846d64943d0553de6900553
Aplles <94980379+Aplles@users.noreply.github.com>
2021-11-30 19:36:46 +0300

    Add files via upload

--- task_06.py
+++ task_06.py
@@ -7,39 +7,34 @@ class NoSuchStrategyError(Exception):
 
 
 def rps_game_winner(array):
-    try:
-        if len(array) < 3:
-            dict = {}
-            for i in array:
-                dict[i[1]] = i[0]
-            if len(dict) == 1:
+    if len(array) < 3:
+        dict = {}
+        for i in array:
+            dict[i[1]] = i[0]
+        if len(dict) == 1:
+            return " ".join(array[0])
+        elif len(dict) < 3:
+            keys = list(dict.keys())
+            if keys[0] == 'P' and keys[1] == 'S':
+                return " ".join(array[1])
+            elif keys[0] == 'S' and keys[1] == 'P':
                 return " ".join(array[0])
-            elif len(dict) < 3:
-                keys = list(dict.keys())
-                if keys[0] == 'P' and keys[1] == 'S':
-                    return " ".join(array[1])
-                elif keys[0] == 'S' and keys[1] == 'P':
-                    return " ".join(array[0])
-                elif keys[0] == 'P' and keys[1] == 'R':
-                    return " ".join(array[0])
-                elif keys[0] == 'R' and keys[1] == 'P':
-                    return " ".join(array[1])
-                elif keys[0] == 'S' and keys[1] == 'R':
-                    return " ".join(array[1])
-                elif keys[0] == 'R' and keys[1] == 'S':
-                    return " ".join(array[0])
-                else:
-                    raise NoSuchStrategyError
-        else:
-            raise WrongNumberOfPlayersError
-    except WrongNumberOfPlayersError:
-        return 'WrongNumberOfPlayersError'
-    except NoSuchStrategyError:
-        return 'NoSuchStrategyError'
+            elif keys[0] == 'P' and keys[1] == 'R':
+                return " ".join(array[0])
+            elif keys[0] == 'R' and keys[1] == 'P':
+                return " ".join(array[1])
+            elif keys[0] == 'S' and keys[1] == 'R':
+                return " ".join(array[1])
+            elif keys[0] == 'R' and keys[1] == 'S':
+                return " ".join(array[0])
+            else:
+                raise NoSuchStrategyError
+    else:
+        raise WrongNumberOfPlayersError
 
 
-(rps_game_winner([['player1', 'R'], ['player2', 'S']]))
-print(rps_game_winner([['player1', 'P'], ['player2', 'S']]))
-print(rps_game_winner([['player1', 'P'], ['player2', 'P']]))
-print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))
-print(rps_game_winner([['player1', 'P'], ['player2', 'A']]))
+rps_game_winner([['player1', 'R'], ['player2', 'S']])
+rps_game_winner([['player1', 'P'], ['player2', 'S']])
+rps_game_winner([['player1', 'P'], ['player2', 'P']])
+rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']])
+rps_game_winner([['player1', 'P'], ['player2', 'A']])
